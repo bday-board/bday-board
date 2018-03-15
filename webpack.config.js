88f1967b02a80c250844c54b0d924c3460399cb8
Ruslan <gt12356@gmail.com>
2018-03-15 17:22:35 +0300

    прототип

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const webpack = require("webpack");
+const path = require('path');
+const NODE_ENV = 'production';
+//const NODE_ENV = 'development';
+
+const analyzeBundles = false;
+
+let config = {
+	entry: {
+		app: './src/app/app.jsx'
+	},
+
+	output: {
+		path: path.resolve(__dirname, "js/dist/"),
+		filename: '[name].min.js'
+	},
+	resolve: {
+		extensions: ['.js', '.jsx']
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: ['babel-loader']
+			},
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader']
+			}
+		]
+	},
+	plugins: [
+		new webpack.DefinePlugin({
+			'process.env.NODE_ENV': JSON.stringify(NODE_ENV)
+		}),
+		new webpack.ContextReplacementPlugin(/moment[\\/]locale$/, /^\.\/ru$/)
+	]
+};
+if(NODE_ENV == 'production') {
+	config.plugins.push(new webpack.optimize.UglifyJsPlugin());
+	config.plugins.push(new webpack.optimize.ModuleConcatenationPlugin());
+}
+else {
+	config.plugins.push(new webpack.NamedModulesPlugin());
+	config.devtool = 'eval-source-map';
+	config.watchOptions = {
+		ignored: /node_modules/
+	};
+
+}
+
+if(analyzeBundles) {
+	const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+	config.plugins.push(new BundleAnalyzerPlugin());
+}
+module.exports = config;
