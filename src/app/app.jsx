1b203f34622e88a3b4ba680fe385b9458f8f588f
Ruslan <gt12356@gmail.com>
2018-03-29 17:15:24 +0300

    увеличен интервал для карусели

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -29,7 +29,7 @@ class App extends Component {
 				this.setState({users: xhr.response}, () => {
 					let options = {
 						autoplay: true,
-						autoplayTimeout: 5000,
+						autoplayTimeout: 8000,
 						mouseDrag: true,
 						center: true,
 						items: 3,
